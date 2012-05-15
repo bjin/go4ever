175fe313b9da8601cc754b690d981373ec1cc610
Xiangru Huang <m1a21117@gmail.com>
2012-05-15 15:53:42 +0800

    finale update

--- uct.cpp
+++ uct.cpp
@@ -9,20 +9,23 @@
 
 void init_node(node* n)
 {
-    n->nb = 0;
+
+    n->simple.initial();
+    n->rave.initial();
     n->child = NULL;
     n->brother = NULL;
-    n->value = 0;
 }
 
 static board_t* root_board = new board_t, *temp_board = new board_t;
 static bool print = false, print2 = false;
-static int *temp_moves = new int[1000];
+static int *temp_moves = new int[moves_total*2+1];
 static stone_t global_color;
-static index_t move_rec[1000];
-static stone_t color_rec[1000];
+static index_t move_rec[steps_total+10];
+static stone_t color_rec[steps_total+10];
 static bool move_set[moves_total*2+1];
 static bool move_set_to[moves_total*2+1];
+static index_t move_total[moves_total*2+1];
+static index_t move_score[moves_total*2+1];
 static node *root = NULL;
 static int step;
 static node* node_list[max_depth];
@@ -74,23 +77,36 @@ void update_rave(int nodes_num, float_num value)
         loop_node = temp_node->child;
         while (loop_node != NULL){
             if (move_set[get_id(color_rec[k+1],loop_node->move)]) {
-                loop_node->nb++;
-                loop_node->value += subjective_score(oppo_color(color_rec[k+1]),value);
+                //update_count_rave(loop_node,subjective_score(oppo_color(color_rec[k+1]),value));
+                loop_node->rave.add_sample(subjective_score(oppo_color(color_rec[k+1]),value));
             }
             loop_node = loop_node->brother;
         }
     }
+
+}
+
+void update_winrate(float_num value)
+{
+    int last = (int)(step * coeff);
+    for (int i = 1; i < last; i++) {
+        index_t move_id = get_id(color_rec[i], move_rec[i]);
+        move_total[move_id]++;
+        move_score[move_id]+=subjective_score(color_rec[i],value);
+    }
+}
+
+float_num consider_winrate(float_num value, index_t move_id)
+{
+    if (move_total[move_id] == 0)
+        return value;
+    return value + ((float_num)move_score[move_id])/move_total[move_id];
 }
 
 //  find a child node of bi, which has the maximum value
 node* descend_by_UCB1(node* n,stone_t color)
 {
-    int total = 0;
-    node* temp = n->child;
-    while (temp != NULL) {
-        total = total + temp->nb;
-        temp = temp->brother;
-    }
+    int total = n->simple.count;
     float_num max_value = 0;
     node* max_index = NULL;
     bool found = false;
@@ -100,13 +116,19 @@ node* descend_by_UCB1(node* n,stone_t color)
     else
         log_total_m2 = log(total);
     float_num value;
-    temp = n->child;
+    node *temp = n->child;
     while (temp != NULL) {
-        int vt = temp->nb;
+        int vt = temp->simple.count+temp->rave.count;
         if (vt == 0){
             value = 10000+fast_irandom(1000);
         } else {
-            value = -(temp->value)/vt+sqrt((log_total_m2)/vt);
+            value = -temp->mix_value(log_total_m2,0.5,0.01);//+sqrt((log_total_m2)/vt)*tree_search_coeff;
+            //gtp_printf("move=%d,\tvalue=%f\n",temp->move,value);
+            //value = -(temp->value)/vt+sqrt((log_total_m2)/vt)*tree_search_coeff;
+            if (consider_winrate_bool) {
+                index_t move_id = get_id(color,temp->move);
+                value = consider_winrate(value,move_id);
+            }
         }
         if ((!found) || max_value < value) {
             max_index = temp;
@@ -115,16 +137,15 @@ node* descend_by_UCB1(node* n,stone_t color)
         }
         temp = temp->brother;
     }
-    if (max_index == NULL)
-        gtp_printf("\t%lf",max_value);
     return max_index;
 }
 
-
-static bool xxx = true;
+static double deps = 0;
+static double dep_count = 0;
 
 void play_one_sequence(node* root,stone_t color)
 {
+
     fork_board(temp_board,root_board);
     int i = 0;
     node_list[0] = root;
@@ -139,28 +160,27 @@ void play_one_sequence(node* root,stone_t color)
         color = oppo_color(color);
         i++;
     }while (node_list[i]->child != NULL);
+    deps += i;
+    dep_count++;
+    //gtp_printf("depth=\t%d\t%d\n",i,node_list[1]->move);
     create_node(temp_board, node_list[i], color);
     float_num white_value = get_value_by_MC(temp_board,color);
-    /*if (xxx){
-    for (int kk=0; kk <= step; kk++){
-        gtp_printf("%d,\t%c\n",move_rec[kk],stone2char(color_rec[kk]));
-    }
-    xxx= false;
-    }*/
-    //gtp_printf("\t%f\n",subjective_score(color,white_value));
-    node_list[i]->value = subjective_score(color,white_value);
-    node_list[i]->nb = 1;
-    //gtp_printf("%f\n",node_list[i]->value);
-    update_value(i,neg_value(node_list[i]->value));
+    //node_list[i]->value = subjective_score(color,white_value);
+    //node_list[i]->nb = 1;
+    //update_value(i, -node_list[i]->value);
+    update_value(i, -subjective_score(color,white_value));
     update_rave(i, white_value);
+    if (consider_winrate_bool)
+        update_winrate(white_value);
 }
 
 void update_value(int depth, float_num value)
 {
     for (int i = depth - 1; i >= 0; i--) {
-        node_list[i]->value = node_list[i]->value + value;
-        node_list[i]->nb++;
-        value = neg_value(value);
+        node_list[i]->simple.add_sample(value);
+        //node_list[i]->value = node_list[i]->value + value;
+        //node_list[i]->nb++;
+        value = -value;
     }
 }
 
@@ -168,7 +188,6 @@ void create_node(board_t* b, node* n,stone_t color)
 {
     if (n->child != NULL)
         return;
-    n->nb++;
     int len = gen_moves(b, color, temp_moves, true);
     for (int i = 0; i < len; i++){
         if (is_eyelike_3x3(b->nbr3x3[temp_moves[i]],color)) continue;
@@ -206,7 +225,7 @@ float_num get_value_by_MC(board_t* temp_board,stone_t next_color)
         black_passed = false;
         white_passed = false;
         color = next_color;
-        while((!black_passed || !white_passed) && step < 500) {
+        while((!black_passed || !white_passed) && step < steps_total) {
             pos = color == STONE_WHITE && white_passed ||
                     color == STONE_BLACK && black_passed ? -1 : gen_move(gb, color);
             if (pos >= 0) {
@@ -223,15 +242,20 @@ float_num get_value_by_MC(board_t* temp_board,stone_t next_color)
             }
         }
         calc_final_score(gb, bs, ws);
-
+        return ws+6.5-bs;
         if(bs > ws + 6.5){
             bcnt= bcnt + 1;
         }
         else {
-            wcnt= wcnt+ 1;
+            wcnt= wcnt + 1;
         }
     }
-    return (wcnt)/(wcnt+bcnt);
+    return ws+6.5-bs;
+    //return (wcnt)/(wcnt+bcnt);
+    /*if (wcnt > 0.01)
+        return 1;
+    else
+        return 0;*/
 }
 
 void clean_subtree(node* n)
@@ -246,26 +270,55 @@ void clean_subtree(node* n)
     delete n;
 }
 
+static int steps = 0;
+
 index_t next_move(node* root, stone_t color, index_t pre_move)
 {
+    steps++;
     fork_board(temp_board, root_board);
     create_node(temp_board, root, color);
-    double time_out = clock() + 4 * CLOCKS_PER_SEC;
+    double time_out = clock() + 8 * CLOCKS_PER_SEC;
     int time_count = 0;
+    deps = 0; dep_count = 0;
     while (clock() < time_out) {
-        //time_out--;
         step = 0;
         play_one_sequence(root, color);
         time_count++;
-
+        //if (time_count > 5) break;
     }
     node *max_node = NULL;
-    float_num max_value = 0;
+    float_num max_value = -100000;
     float_num tmp;
     node *temp = root->child;
     while (temp != NULL){
+        //tmp = -temp->simple.sum/temp->simple.count;
+        tmp = -temp->simple.calc_mean()*coeff_simple-temp->rave.calc_mean()*coeff_rave;
+        if (consider_winrate_bool){
+            index_t move_id = get_id(color,temp->move);
+            tmp = consider_winrate(tmp,move_id);
+        }
+        //gtp_printf("move = %d,\tvalue= %f,\tnum=%d,\tvariance=%f\n",temp->move,temp->simple.sum,temp->simple.count,temp->rave.calc_variance());
+        float_num d = root_board->prob_sum2[color-1][temp->move];
+        if (d > 0){
+            if (steps < 50)
+                d = log(d)* 0.01 + 1;
+            else
+                d = log(d)* 0.02 + 1;
+        } else {
+            d = 0.001;
+        }
+        if (tmp > 0)
+            tmp = tmp * d;
+        else
+            tmp = tmp / d;
+        /*if (on_edge(temp->move) && steps < 20){
+            if (tmp > 0){
+                tmp = tmp * 0.95;
+            } else {
+                tmp = tmp * 1.05;
+            }
 
-        tmp = -temp->value/temp->nb;
+        }*/
         if (max_node == NULL || max_value < tmp){
             max_node = temp;
             max_value = tmp;
@@ -287,12 +340,18 @@ index_t next_move(node* root, stone_t color, index_t pre_move)
         return -1;
     max_node ->brother = NULL;
     root = max_node;
+    gtp_printf("result=%d\n",max_node->move);
+    gtp_printf("\tsimple: count=%d/%d,\tmean=%f\n",max_node->simple.count,time_count,max_node->simple.calc_mean());
+    gtp_printf("\trave: count=%d,\tmean=%f\n",max_node->rave.count,max_node->rave.calc_mean());
+    gtp_printf("\tavg_depth=%f,\t%f/%f\n",deps/dep_count,deps,dep_count);
     return max_node->move;
 }
 
 index_t next_move(board_t* b,stone_t color,index_t pre_move)
 {
-    /*if (root == NULL || pre_move == -1){
+    memset(move_score,0,sizeof(move_score));
+    memset(move_total,0,sizeof(move_total));
+    if (root == NULL || pre_move == -1){
         root = new node;
         init_node(root);
     } else {
@@ -300,7 +359,7 @@ index_t next_move(board_t* b,stone_t color,index_t pre_move)
         root = NULL;
         while (temp != NULL){
             if (temp->move == pre_move){
-                gtp_printf("found pre move%d\n",temp->nb);
+                //gtp_printf("found pre move%d\n",temp->nb);
                 root = temp;
                 break;
             }
@@ -310,14 +369,12 @@ index_t next_move(board_t* b,stone_t color,index_t pre_move)
             root = new node;
             init_node(root);
         }
-    }*/
-    root = new node;
-    init_node(root);
+    }
     print = true; print2 = true;
     fork_board(root_board,b);
     global_color = color;
     index_t ans = next_move(root,global_color,pre_move);
-    gtp_printf("%f\n",b->prob_sum2[color-1][pos]);
+    gtp_printf("\t%f\n",b->prob_sum2[color-1][pos]);
     return ans;
 }
 
--- uct.hpp
+++ uct.hpp
@@ -2,7 +2,7 @@
 #define UCT_H
 
 #include "board.hpp"
-
+#include <cmath>
 #define max_board 100000
 #define max_depth 500
 #define null (-1)
@@ -11,13 +11,77 @@
 #define edge_width 2
 #define coeff 0.75
 #define moves_total 250
+#define steps_total 350
+#define tree_search_coeff 1
+#define consider_winrate_bool false
+#define coeff_simple 1
+#define coeff_rave 0.3
+
+struct stat {
+    float_num sum,square_sum;
+    int count;
+    float_num calc_mean()
+    {
+        if (count == 0)
+            return 0;
+        else
+            return (sum/count);
+    }
+    float_num calc_variance(){
+        if (count == 0) return 0;
+        float_num mean = calc_mean();
+        return (square_sum/count)-mean*mean;
+    }
+    void add_sample(float_num value){
+        count++;
+        sum += value;
+        square_sum += value*value;
+    }
+    void initial(){
+        count = 0;
+        sum = 0;
+        square_sum = 0;
+    }
+};
 
 struct node {
     node* child;
     node* brother;
-    float_num value;
-    int nb;
+    stat simple, rave;
+    //float_num value,rave_value;
+    //int nb,rave_nb;
     index_t move;
+    //float_num sum,rave_sum;
+    float_num mix_value(float_num log_total, float_num mix_ratio1, float_num mix_ratio2){
+
+        stat stat1 = simple;
+        stat stat2 = rave;
+
+        float b1 = mix_ratio1;
+        float b2 = mix_ratio2;
+        float n1 = stat1.count;
+        float n2 = stat2.count;
+
+        float s1 = stat1.sum;
+        float s2 = stat2.sum;
+
+        float v1 = stat1.square_sum;
+        float v2 = stat2.square_sum;
+
+        float nn1 = n1 * n1;
+        float nn2 = n2 * n2;
+
+        float x1 = (v1 + b1*nn1) * n1  -  s1 * s1;
+        float x2 = (v2 + b2*nn2) * n2  -  s2 * s2;
+
+        float t1 = nn1 * x2;
+        float t2 = nn2 * x1;
+        if (fabs(t1*n1+t2*n2) <= 0.0001){
+            return -100000;
+        }
+        float mix = (t1*s1 + t2*s2) / (t1*n1 + t2*n2);
+        return mix;
+    }
 };
 
 
